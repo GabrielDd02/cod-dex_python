5f6203ac0e87b6b5e2870967013878386adf8c49
GabrielDd02 <162463741+GabrielDd02@users.noreply.github.com>
2025-03-19 09:36:26 -0300

    Create iniciais.py

---
+++ iniciais.py
@@ -0,0 +1,7 @@
+#Os comentários são muito importantes na programação porque são usados para documentar o que nosso código faz. Eles também são usados para desativar partes do programa.
+
+print(' GGG   L    ')
+print('G      L    ')
+print('G  GG  L    ')
+print('G   G  L    ')
+print(' GGG   LLLLL')
