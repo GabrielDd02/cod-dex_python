38c793c63a036481faaeac50f80433dd5dd6e432
GabrielDd02 <162463741+GabrielDd02@users.noreply.github.com>
2025-03-19 09:32:28 -0300

    Update padrao.py

--- padrao.py
+++ padrao.py
@@ -1,3 +1,5 @@
+#O Python é executado uma linha de cada vez, de cima para baixo.
+#Podemos gerar várias mensagens usando várias funções. Por exemplo, se quisermos imprimir duas saudações simples:print()
 print('       1')
 print('     2 3')
 print('   4 5 6')
